4a86f5cf8bf1a88975ab1960e8826f4796d3c96c
Hector Vargas <vargash1@wit.edu>
2016-04-09 22:30:07 -0400

    Cam

--- vraspi/cam.py
+++ vraspi/cam.py
@@ -4,7 +4,7 @@
 # @Date:   Saturday, April 9th 2016, 7:59:52 pm
 # @Email:  vargash1@wit.edu
 # @Last modified by:   vargash1
-# @Last modified time: Saturday, April 9th 2016, 9:38:27 pm
+# @Last modified time: Saturday, April 9th 2016, 10:29:52 pm
 import time
 import picamera
 
@@ -25,12 +25,7 @@ class vRaspiCam:
     def takePic(self):
         self.camera.resolution = (2592, 1944)
         self.camera.start_preview()
-        self.camera.exposure_compensation = 2
-        self.camera.exposure_mode = 'spotlight'
-        self.camera.meter_mode = 'matrix'
-        self.camera.image_effect = 'gpen'
         # Give the cam some time to adjust to conditions
-        time.sleep(2)
         try:
             self.camera.capture('foo.jpg')
             self.camera.stop_preview()
