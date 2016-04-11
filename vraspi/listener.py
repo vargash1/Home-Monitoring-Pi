df7d89f1013b7ce7d564aca4499e074af7ee3799
Hector Vargas <vargash1@wit.edu>
2016-04-11 00:26:37 -0400

    :coffee: Derp

--- vraspi/listener.py
+++ vraspi/listener.py
@@ -4,20 +4,22 @@
 # @Date:   Sunday, April 10th 2016, 11:18:37 pm
 # @Email:  vargash1@wit.edu
 # @Last modified by:   vargash1
-# @Last modified time: Sunday, April 10th 2016, 11:34:19 pm
+# @Last modified time: Monday, April 11th 2016, 12:26:28 am
 import multiprocessing
-from vraspi import ultrasonic, motion
+from vraspi import ultrasonic, motion, light
 import time
 
 
 def main():
     ultratest = ultrasonic.UltraSonicSensor()
     motiontest = motion.MotionSensor()
-
+    lighttest = light.LightSensor()
     p1 = multiprocessing.Process(target=motiontest.detect_Motion)
     p2 = multiprocessing.Process(target=ultratest.detect_dist)
+    p3 = multiprocessing.Process(target=lighttest.detect_light)
     p1.start()
     p2.start()
+    p3.start()
 
 if __name__ == "__main__":
     main()
