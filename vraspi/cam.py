58064f9a8892e5ffc4ff04285b27fd62870a472b
Hector Vargas <vargash1@wit.edu>
2016-04-12 06:07:49 -0400

    :coffee: Camera

--- vraspi/cam.py
+++ vraspi/cam.py
@@ -4,7 +4,7 @@
 # @Date:   Saturday, April 9th 2016, 7:59:52 pm
 # @Email:  vargash1@wit.edu
 # @Last modified by:   vargash1
-# @Last modified time: Tuesday, April 12th 2016, 5:52:59 am
+# @Last modified time: Tuesday, April 12th 2016, 6:06:29 am
 import time
 import picamera
 
@@ -26,6 +26,7 @@ class vRaspiCam:
     """Takes a picture"""
     def take_Pic(self):
         self.camera.resolution = (2592, 1944)
+        camera.start_preview()
         time.sleep(2)
         # Give the cam some time to adjust to conditions
         try:
